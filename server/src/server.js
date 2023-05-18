dca78ba2068c5ec55b7f3714b3b942bccb304258
Margus Liinev <margus.liinev@gmail.com>
2023-05-18 21:58:17 +0300

    deployment attempt 1

--- server/src/server.js
+++ server/src/server.js
@@ -1,19 +1,30 @@
 import express from 'express';
+const app = express();
+import dotenv from 'dotenv';
+dotenv.config();
 import cors from 'cors';
 import 'express-async-errors';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
 import { errorHandler } from './middleware/errorHandler.js';
 import { notFound } from './middleware/notFound.js';
 import router from './routes/index.js';
-import dotenv from 'dotenv';
-dotenv.config();
 
-const app = express();
-const port = process.env.PORT || 5000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.use(express.json());
 app.use(cors());
 app.use('/', router);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
 app.use(errorHandler);
 app.use(notFound);
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, console.log(`Server is listening on port ${port}`));
