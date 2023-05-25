d1aae7a4d9338b8b93e6533d981c742c94d1dce8
Margus Liinev <margus.liinev@gmail.com>
2023-05-25 15:23:19 +0300

    server side validation when registering, password hashing

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,17 +1,50 @@
 import { Request, Response } from 'express';
 import { query } from '../db/index';
 import { BadRequestError } from '../errors';
+import bcrypt from 'bcryptjs';
+
+const usernameRegex = /^[A-Za-z0-9]{3,16}$/;
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!@#$%&*,.?]{8,}$/;
 
 const register = async (req: Request, res: Response) => {
     const { username, email, password } = req.body;
+
     if (!username || !email || !password) {
         throw new BadRequestError('Missing username, email or password');
     }
     const uniqueEmail = await query('select email from users where email = $1', [email]);
-    if (uniqueEmail) {
+    if (uniqueEmail.length >= 1) {
         throw new BadRequestError('Email address is already registered.');
     }
-    const user = await query('insert into users (username, email, password) values ($1, $2, $3) returning *', [req.body.username, req.body.email, req.body.password]);
+
+    if (!usernameRegex.test(username)) {
+        if (username.length < 3 || username.length > 16) {
+            throw new BadRequestError('Invalid username, username must be between 3-16 characters.');
+        } else {
+            throw new BadRequestError('Invalid username, username can only contain letters (A-Z) and numbers (0-9).');
+        }
+    }
+
+    if (!emailRegex.test(email)) {
+        throw new BadRequestError('Email is invalid');
+    }
+
+    if (!passwordRegex.test(password)) {
+        if (password.length < 8) {
+            throw new BadRequestError('Password must be at least 8 characters long.');
+        } else if (!/(?=.*[a-z])/.test(password)) {
+            throw new BadRequestError('Password must contain at least one letter.');
+        } else if (!/(?=.*\d)/.test(password)) {
+            throw new BadRequestError('Password must contain at least one number.');
+        } else {
+            throw new BadRequestError('Allowed special characters in password: !@#$%&*,.?');
+        }
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    const user = await query('insert into users (username, email, password) values ($1, $2, $3) returning *', [req.body.username, req.body.email, hash]);
     res.status(201).json({ user });
 };
 
