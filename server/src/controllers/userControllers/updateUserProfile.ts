79f3b7d83db4b0505bba898e16bb1e99f764f4de
Margus Liinev <margus.liinev@gmail.com>
2023-07-03 22:26:56 +0300

    fixed bug with ID being removed from user after profile update

--- server/src/controllers/userControllers/updateUserProfile.ts
+++ server/src/controllers/userControllers/updateUserProfile.ts
@@ -47,6 +47,7 @@ export const updateUserProfile = async (req: AuthenticatedRequest, res: Response
 
     const result = await query('UPDATE users SET username = $1, email = $2 WHERE id = $3 returning *', [username, email, req.user.userId]);
     const user = {
+        id: result[0].id as string,
         username: result[0].username as string,
         email: result[0].email as string,
     };
