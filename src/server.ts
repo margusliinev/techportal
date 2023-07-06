72a86d27358cf27fb0d053951a071a834a8479b5
Margus Liinev <margus.liinev@gmail.com>
2023-07-06 14:22:04 +0300

    switched to shared Monorepo to simplify deployment

--- src/server.ts
+++ src/server.ts
@@ -42,9 +42,9 @@ app.use('/', statsRouter);
 app.use('/', skillsRouter);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.resolve(__dirname, '../../client/dist')));
+    app.use(express.static(path.resolve(__dirname, '../client/dist')));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../../client/dist', 'index.html'));
+        res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
     });
 }
 
